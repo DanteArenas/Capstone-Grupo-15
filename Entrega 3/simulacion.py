14c3be8a9f6ab656319b2421c9e032466b6c8565
DANTE ARENAS <dante.arenas@uc.cl>
2025-06-18 00:18:57 -0400

    simulacion 40 dias 1

--- Entrega 3/simulacion.py
+++ Entrega 3/simulacion.py
@@ -1,7 +1,8 @@
 # este archivo juntará todo, se llamarán a las funciones de cada archivo y se generarán las simulaciones
 from ruteo import generar_rutas, graficar_rutas, mejorar_rutas_2_opt, caso_base_ruteo
-from pricing import procesar_datos_de_distancia, generar_matriz_ck, resolver_precio_optimo
+from pricing import procesar_datos_de_distancia, generar_matriz_ck, resolver_precio_optimo, resolver_precio_optimo_zona, zona_vehiculo
 from kpis import utilidad_total, distancia_promedio, utilidad_de_entregas, ventas_perdidas, demanda_online_insatisfecha, creacion_df_40_dias_ruteo
+import pandas as pd
 import os
 
 base_dir = os.path.dirname(__file__)
@@ -11,8 +12,16 @@ path_flota = os.path.join(base_dir, '..', 'Datos', 'flota_20250115.csv')
 path_camiones = os.path.join(base_dir, '..', 'Datos', 'vehiculos_20250115.csv')
 path_productos = os.path.join(
     base_dir, '..', 'Datos', 'productos_20250115.csv')
+path_stock_diario = os.path.join(
+    base_dir, '..', 'Datos', 'analisis de datos', 'stock_diario.csv')
+path_stock_diario_caso_base = os.path.join(
+    base_dir, '..', 'Datos', 'analisis de datos', 'stock_diario_caso_base.csv')
 
-n_dias = 1
+df_stock_diario = pd.read_csv(path_stock_diario)
+df_stock_diario_caso_base = pd.read_csv(path_stock_diario_caso_base)
+df_zonas = pd.read_csv(path_zonas)
+
+n_dias = 40
 
 for dia in range(1, n_dias + 1):
     path_venta_zona = os.path.join(
@@ -41,8 +50,16 @@ for dia in range(1, n_dias + 1):
     matriz_ck = generar_matriz_ck(
         df_distancias_ordenada, data_resultados_mejorada)
 
+    # Obtener df_zona_vehiculo
+    df_zona_vehiculo = zona_vehiculo(data_resultados_mejorada, df_zonas)
+
     # Resolver precio óptimo
-    df_precios_optimos = resolver_precio_optimo(matriz_ck)
+    path_demanda_digital = os.path.join(
+        base_dir, 'ventas_digitales_estocasticas', f'venta_zona_estocastica_dia_{dia}.csv')
+    df_demanda_digital = pd.read_csv(path_demanda_digital)
+
+    df_precios_optimos = resolver_precio_optimo_zona(
+        matriz_ck, df_zona_vehiculo, df_stock_diario, df_demanda_digital, df_zonas, dia)
 
     # Guardar resultados
     path_resultados = os.path.join(
@@ -53,23 +70,7 @@ for dia in range(1, n_dias + 1):
 
     df_precios_optimos.to_csv(path_resultados, index=False)
 
-    # Caso base
-    data_resultados_caso_base = caso_base_ruteo(path_zonas, path_tiendas, path_venta_zona,
-                                                path_flota, path_camiones, path_productos, dia)
-    # Graficar rutas de caso base
-
-    resultados_caso_base = os.path.join(
-        base_dir, 'resultados', f'dia_{dia}', 'caso_base', f'resultados_caso_base_dia_{dia}.csv')
-    graficar_rutas(data_resultados_caso_base, path_zonas, path_tiendas,
-                   dia, caso_base=True)
-
 # KPIs
-path_distancias_dias_caso_base = creacion_df_40_dias_ruteo(
-    n_dias, caso_base=True, mejorados=False, cw_solo=False)
-distancias_promedio_caso_base = distancia_promedio(
-    path_distancias_dias_caso_base)
-print(f"Distancia promedio caso base: {distancias_promedio_caso_base}")
-
 path_distancias_dias_cw_solo = creacion_df_40_dias_ruteo(
     n_dias, caso_base=False, mejorados=False, cw_solo=True)
 distancias_promedio_cw_solo = distancia_promedio(path_distancias_dias_cw_solo)
